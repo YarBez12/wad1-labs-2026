d479baa54864fd3e1dd2c407cdeca7e4f37d1e27
YarBez12 <yaroslav.bezvesilnyi@nure.ua>
2026-02-10 11:47:01 +0000

    Add Models And Update Start Controller

--- controllers/start.js
+++ controllers/start.js
@@ -1,11 +1,15 @@
 'use strict'
 
 import logger from "../utils/logger.js";
+import appStore from "../models/app-store.js";
 
 const start = {
     createView(request, response) {
+        const info = appStore.getAppInfo();
+        logger.debug(info);
+
         logger.info("Start page loading!");
-        response.send("Welcome to Yaroslav's Playlist app!");
+        response.json(info);
     },
 };
 
