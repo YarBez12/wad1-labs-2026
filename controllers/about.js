2089c225ec9eeff36375081fab4ad47d0c8660b5
YarBez12 <yaroslav.bezvesilnyi@nure.ua>
2026-02-10 12:31:09 +0000

    Add About Page

--- controllers/about.js
+++ controllers/about.js
@@ -1,11 +1,16 @@
 'use strict'
 
 import logger from "../utils/logger.js";
+import employees from "../models/employee.js";
 
 const about = {
     createView(request, response) {
         logger.info("About page loading!");
-        response.send("About the Playlist app");
+        const viewData = {
+          title: "Playlist App About",
+          info: employees.getAppInfo()
+        };
+        response.render('about', viewData);
     },
 };
 
